8f653f8e005f42c8c059160a885eba36f7dfa7ae
eva-hauptmann <113628281+eva-hauptmann@users.noreply.github.com>
2022-11-23 09:03:10 -0600

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,32 +1,35 @@
-function findAuthorById(authors, id) {
+function findAuthorById(accounts, id) {
   let foundId = accounts.find((account) => account.id === id);
   return foundId;
-}
-
-function findBookById(books, id) {
-  let foundBookId = books.find((book) => book.id === id);
-  return foundBookId
-}
-
-function partitionBooksByBorrowedStatus(books) {
-  let returned = books.filter((book) =>
-  book.borrows.every((borrow) => borrow.returned === true))
-
-  let borrowed = books.filter((book) => 
-  book.borrows.some((borrow) => borrow.returned === false))
-
-  let results = [[...borrowed], [...returned]]
-  return results
-}
-
-function getBorrowersForBook(book, accounts) {
+ } 
+ 
+ 
+ function findBookById(books, id) {
+   let foundBookId = books.find((book) => book.id === id);
+   return foundBookId
+ }
+ 
+ function partitionBooksByBorrowedStatus(books) {
+   let returned = books.filter((book) =>
+   book.borrows.every((borrow) => borrow.returned === true))
+ 
+   let borrowed = books.filter((book) => 
+   book.borrows.some((borrow) => borrow.returned === false))
+ 
+   let results = [[...borrowed], [...returned]]
+   return results
+ }
+ 
+ function getBorrowersForBook(book, accounts) {
   return book.borrows
-  .map((borrow)=> {
-    let acct = accounts.find((account) => account.id === borrow.id)
-    return {...borrow, ...account}
-  })
-  .slice(0, 10)
-}
+   .map((borrow) => {
+    let account = accounts.find((account) => account.id === borrow.id);
+    return { ...borrow, ...account };
+   })
+     .slice(0, 10);
+ }
+ 
+ 
 
 module.exports = {
   findAuthorById,
