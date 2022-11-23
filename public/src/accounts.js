db9257d0625f111ba03ac94196e4307ac8d5ccf0
eva-hauptmann <113628281+eva-hauptmann@users.noreply.github.com>
2022-11-23 09:02:37 -0600

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -3,18 +3,13 @@ function findAccountById(accounts, id) {
   let found = accounts.find((account) => account.id === id);
 
   return found;
-
  }
- 
- 
  function sortAccountsByLastName(accounts) {
   accounts.sort((a, b) =>
    a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1
   );
   return accounts;
  }
- 
- 
 
  
  function getTotalNumberOfBorrows(account, books) {
@@ -30,33 +25,19 @@ function findAccountById(accounts, id) {
  }
 
 
- function getBooksPossessedByAccount(account, books, authors) {
-  let result = [];
-  let borrowMatch = [];
-  books.forEach((item) => {
-   const borrowed = item.borrows;
-   const book = {
-    id: item.id,
-    title: item.title,
-    genre: item.genre,
-    authorId: item.authorId,
-    author: {},
-    borrows: {}
-   };
-   const { id, title, genre, authorId, author, borrows } = book;
- 
-   borrowed.forEach((borrow) => {
-    if (borrow.id === account.id && borrow.returned === false) {
-     result.push(book);
-     borrowMatch.push(borrow);
-     book.borrows = borrowMatch;
-     book.author = authors.filter((auth) => auth.id === book.authorId)[0];
-    }
-   });
-  });
-  return result;
- }
+function getBooksPossessedByAccount(account, books, authors) {
+let checkedOutBooks = []
+books.forEach(book => {
+  if(book.borrows.find(borrow => borrow.id === account.id && borrow.returned === false)){
+    checkedOutBooks.push(book)
+  }})
+checkedOutBooks.forEach(book => {
+  let authorInfo = authors.find(author => author.id === book.authorId)
+  book['author'] = authorInfo
+})
 
+return checkedOutBooks
+}
 
 module.exports = {
   findAccountById,
@@ -64,3 +45,4 @@ module.exports = {
   getTotalNumberOfBorrows,
   getBooksPossessedByAccount,
 };
+
